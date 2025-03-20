1009fca83e1dd603dfd4ee3766b3796ade0940db
Lams1474 <la.maiguas@uea.edu.ec>
2025-03-19 21:01:53 -0500

    Merge remote-tracking branch 'origin/main'

--- MI PRACTICA DE PYTHON DESDE CERO/BASICO/00_helloworld.py
+++ MI PRACTICA DE PYTHON DESDE CERO/BASICO/00_helloworld.py
@@ -3,3 +3,25 @@
 #Nuestro famoso Hola Mundo en Python#
 
 print("hola Luis")
+
+# Esto es un comentario
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este también es un
+comentario
+en varias líneas
+'''
+
+# Cómo consultar el tipo de dato
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
